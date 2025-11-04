db12583b722f6e49f99f731851a83004039b72f6
DeTiveNC <116792124+detivenc@users.noreply.github.com>
2025-11-04 14:16:15 +0100

    New: Add starter for freecodecamp

---
+++ starter/freecodecamp/scripts/hello.exs
@@ -0,0 +1,3 @@
+defmodule Hello do
+  def world(name), do: IO.puts("Hello #{name}")
+end
---
+++ starter/freecodecamp/stats/lib/central_tendency/median.ex
@@ -0,0 +1,38 @@
+defmodule Stats.CentralTendency.Median do
+  require Integer
+
+  alias Stats.{Errors, Validators}
+
+  @spec median([number()]) :: number() | {:error, String.t()}
+  def median(num_list) when is_list(num_list) do
+    num_list
+    |> Validators.validate_num_list()
+    |> calc_median()
+  end
+
+  def median(_), do: Errors.invalid_data_types()
+
+  # ====================================================================
+
+  defp calc_median({:error, _msg}), do: Errors.invalid_data_types()
+  defp calc_median({false, _}), do: Errors.invalid_data_types()
+
+  defp calc_median({true, num_list}) do
+    count = Enum.count(num_list)
+
+    num_list
+    |> Enum.sort()
+    |> get_median(Integer.is_even(count), count)
+  end
+
+  # ====================================================================
+
+  defp get_median(num_list, false, count), do: Enum.at(num_list, div(count, 2))
+
+  defp get_median(num_list, true, count) do
+    a = Enum.at(num_list, div(count - 1, 2))
+    b = Enum.at(num_list, div(count, 2))
+
+    (a + b) / 2
+  end
+end
